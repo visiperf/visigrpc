214fe583f3f9903a9cfe8ea0320dde118087d442
Romain Jeannoutot <romain@visiperf.io>
2020-01-22 11:34:33 +0100

    use one func to intercept panic

--- panic.interceptor.go
+++ panic.interceptor.go
@@ -1,30 +1,21 @@
 package visigrpc
 
 import (
-	"context"
 	"errors"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
-func UnaryPanicInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	defer func() {
-		rec := recover()
-		if rec != nil {
-			var e error
+func RecoveryHandler(p interface{}) error {
+	var e error
 
-			switch t := rec.(type) {
-			case string:
-				e = errors.New(t)
-			case error:
-				e = t
-			default:
-				e = errors.New(`unknown error`)
-			}
+	switch t := p.(type) {
+	case string:
+		e = errors.New(t)
+	case error:
+		e = t
+	default:
+		e = errors.New(`unknown error`)
+	}
 
-			err = Error(codes.Internal, e)
-		}
-	}()
-
-	return handler(ctx, req)
+	return Error(codes.Unknown, e)
 }
