2b9b54f8fd4d5af5e0acf3c001fd055acd69eb59
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2021-10-21 11:47:05 +0200

    feat: add func to retrieve http status code from grpc code

--- status/status.go
+++ status/status.go
@@ -71,3 +71,44 @@ func GrpcCodeFromHttpStatus(status int) codes.Code {
 
 	return codes.Unknown
 }
+
+func HttpStatusFromGrpcCode(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return 200
+	case codes.Canceled:
+		return 499
+	case codes.Unknown:
+		return 500
+	case codes.InvalidArgument:
+		return 400
+	case codes.DeadlineExceeded:
+		return 504
+	case codes.NotFound:
+		return 404
+	case codes.AlreadyExists:
+		return 409
+	case codes.PermissionDenied:
+		return 403
+	case codes.ResourceExhausted:
+		return 429
+	case codes.FailedPrecondition:
+		return 400
+	case codes.Aborted:
+		return 409
+	case codes.OutOfRange:
+		return 400
+	case codes.Unimplemented:
+		return 501
+	case codes.Internal:
+		return 500
+	case codes.Unavailable:
+		return 503
+	case codes.DataLoss:
+		return 500
+	case codes.Unauthenticated:
+		return 401
+	}
+
+	return http.StatusInternalServerError
+}
